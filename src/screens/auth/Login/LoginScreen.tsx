4eb4e25dca91a47ed799c93adbf0b5ca1f59c90a
leedd87 <leedav87@gmail.com>
2024-05-31 09:30:51 -0300

    add form

--- src/screens/auth/Login/LoginScreen.tsx
+++ src/screens/auth/Login/LoginScreen.tsx
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Input, Layout, Text } from '@ui-kitten/components';
 import { CustomIcon } from '../../../common/CustomIcon/CustomIcon';
 
 export const LoginScreen = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
   return (
     <Layout
       style={{
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        paddingHorizontal: 30,
       }}
     >
-      <Text category="h1">Login</Text>
       <Layout style={{ paddingTop: 30 * 0.35 }}>
-        <Text category="h1">Ingresar</Text>
-        <Text category="p2">Por favor, ingrese para continuar</Text>
+        <Text category="h1">Login</Text>
       </Layout>
       <Layout style={{ marginTop: 20 }}>
         <Input
           placeholder="email"
           keyboardType="email-address"
           autoCapitalize="none"
-          //value={form.email}
-          //onChangeText={(email) => setForm({ ...form, email })}
+          value={form.email}
+          onChangeText={(email) => setForm({ ...form, email })}
           style={{ marginBottom: 10 }}
-          //accessoryLeft={<CustomIcon name="email-outline" />}
+          accessoryLeft={<CustomIcon name="email-outline" />}
         />
         <Input
           placeholder="password"
           autoCapitalize="none"
-          //value={form.password}
-          //onChangeText={(password) => setForm({ ...form, password })}
+          value={form.password}
+          onChangeText={(password) => setForm({ ...form, password })}
           secureTextEntry
           style={{ marginBottom: 10 }}
-          //accessoryLeft={<CustomIcon name="lock-outline" />}
+          accessoryLeft={<CustomIcon name="lock-outline" />}
         />
       </Layout>
 
@@ -45,6 +48,10 @@ export const LoginScreen = () => {
       >
         Ingresar
       </Button>
+      <Layout style={{ paddingVertical: 20 }} />
+      <Layout>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+      </Layout>
     </Layout>
   );
 };
